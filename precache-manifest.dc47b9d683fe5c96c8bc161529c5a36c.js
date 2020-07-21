cce7f71231d03f4674c18d9661c7ec194379f625
Gianna Aprile <giannaaprile99@gmail.com>
2020-07-21 18:09:01 -0400

    Updates

---
+++ precache-manifest.dc47b9d683fe5c96c8bc161529c5a36c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fc660f8f94300c4e4ac129f7d696e7",
+    "url": "/Mixified-React/index.html"
+  },
+  {
+    "revision": "4c6449df4f770a1fd765",
+    "url": "/Mixified-React/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "820589feeb9ebac7fdd7",
+    "url": "/Mixified-React/static/css/main.3464905f.chunk.css"
+  },
+  {
+    "revision": "4c6449df4f770a1fd765",
+    "url": "/Mixified-React/static/js/2.654c8b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mixified-React/static/js/2.654c8b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820589feeb9ebac7fdd7",
+    "url": "/Mixified-React/static/js/main.79363d2c.chunk.js"
+  },
+  {
+    "revision": "55ccf8d35ecb00e46e0c",
+    "url": "/Mixified-React/static/js/runtime-main.180b00ac.js"
+  },
+  {
+    "revision": "86ac2f4a8e93ec3bcc38aca60571ec55",
+    "url": "/Mixified-React/static/media/mixifiedLogo.86ac2f4a.png"
+  }
+]);
