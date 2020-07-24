2560c68227e9cf584a97b7c4512894677bc46d75
Gianna Aprile <giannaaprile99@gmail.com>
2020-07-24 11:48:51 -0400

    updated config file to me in prod mode

--- src/config.js
+++ src/config.js
@@ -11,11 +11,11 @@ export const scopes = [
 ];
 
 // ***** REACT CONFIG *****
-//export const redirectUri = "https://gianna-aprile.github.io/Mixified-React/"; // -> To use deployed version of React app
-export const redirectUri = "http://localhost:3000"; // -> To use local version of React app
+export const redirectUri = "https://gianna-aprile.github.io/Mixified-React/"; // -> To use deployed version of React app
+//export const redirectUri = "http://localhost:3000"; // -> To use local version of React app
 
 
 // ***** FLASK APP (API) CONFIG *****
-//export const apiURL = "https://mixified-api.herokuapp.com" // To use deployed version of API
-export const apiURL = "http://127.0.0.1:5000" // To use local version of API
+export const apiURL = "https://mixified-api.herokuapp.com" // To use deployed version of API
+//export const apiURL = "http://127.0.0.1:5000" // To use local version of API
 
